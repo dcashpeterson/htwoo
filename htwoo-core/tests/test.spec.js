e4c8b568baa866aac5f5aee4ffffd313c2efa401
Stefan Bauer <stefan.bauer@n8d.at>
2021-07-28 14:42:16 +0200

    Update test

--- htwoo-core/tests/test.spec.js
+++ htwoo-core/tests/test.spec.js
@@ -1,3 +1,4 @@
+//@ts-check
 const {
     test,
     describe = test.describe,
@@ -6,20 +7,59 @@ const {
     expect
 } = require('@playwright/test');
 const {
+    injectAxe,
+    checkA11y,
+    getViolations,
+    reportViolations
+} = require('axe-playwright');
+const {
     DefaultPage
 } = require('./dev-page.js');
 
-console.log("HJJJJJ");
 
-describe("FIRST TEST", () => {
+describe("Primary Colors:", () => {
 
     test('First Pattern', async ({
         page
     }) => {
         const player = new DefaultPage(page);
         await player.goto("/patterns/atoms-global-primary-colors/atoms-global-primary-colors.rendered.html");
-        expect(await player.page.screenshot()).toMatchSnapshot('primary.png', { threshold: 0.2, fullpage: true });
-        
+        expect(await player.page.screenshot({
+            fullscreen: true
+        })).toMatchSnapshot('primary.png', {
+            threshold: 0.2
+        });
+
+        const snapshot = await page.accessibility.snapshot({
+            interestingOnly: true
+        });
+
+    });
+
+    test('A11Y', async ({
+        page
+    }) => {
+
+        const player = new DefaultPage(page);
+        await player.goto("/patterns/molecules-menus-breadcrumb/molecules-menus-breadcrumb.rendered.html");
+        await injectAxe(player.page);
+
+        const violations = await getViolations(page, 'body', {
+            axeOptions: {
+                runOnly: {
+                    type: 'tag',
+                    values: ['wcag21a', 'wcag21aa', 'best-practice']
+                }
+            }
+        })
+
+        expect(violations.length).toBe(0);
+
+        if (violations.length !== 0) {
+            console.table(violations);
+        }
+
+
     });
 
     test('Second Pattern', async ({
@@ -27,8 +67,10 @@ describe("FIRST TEST", () => {
     }) => {
         const player = new DefaultPage(page);
         await player.goto("/patterns/atoms-global-neutral-colors/atoms-global-neutral-colors.rendered.html");
-        expect(await player.page.screenshot()).toMatchSnapshot('neutral.png', { threshold: 0.2, fullpage: true });
-        
+        expect(await player.page.screenshot()).toMatchSnapshot('neutral.png', {
+            threshold: 0.2
+        });
+
     });
 
 });
